aee29340c5956e6012dd469747ae22293c82e953
Christoph <c.wagner.germany@gmail.com>
2024-10-26 14:57:02 +0200

    bac5

--- kanban_app/views.py
+++ kanban_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 from .dummy_data import gadgets
 
 # Create your views here.
@@ -7,5 +8,5 @@ from .dummy_data import gadgets
 def start_page_view(request):
     return HttpResponse("funtioniert")
 
-def single_gadget_view(request):
-    return HttpResponse(gadgets[0])
+def single_gadget_view(request, gadget_id):
+    return HttpResponse(json.dumps(gadget_id))
