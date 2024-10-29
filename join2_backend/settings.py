14bde6d93c125891a84f008da38d51a536daae39
Christoph <c.wagner.germany@gmail.com>
2024-10-29 14:45:57 +0100

    json1

--- join2_backend/settings.py
+++ join2_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'kanban_app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -53,14 +54,14 @@ MIDDLEWARE = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-  'http://127.0.0.1:5500',
-  'http://localhost:5500',
+  'http://127.0.0.1:4200',
+  'http://localhost:4200',
 ]
 
 
 CORS_ALLOWED_ORIGINS = [
-  'http://127.0.0.1:5500',
-  'http://localhost:5500',
+  'http://127.0.0.1:4200',
+  'http://localhost:4200',
 ]
 
 ROOT_URLCONF = 'join2_backend.urls'
