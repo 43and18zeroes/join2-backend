51099567a01431ca94702eedd461583832e1cb82
Christoph <c.wagner.germany@gmail.com>
2024-11-01 15:32:21 +0100

    PUT

--- kanban_app/api/serializer.py
+++ kanban_app/api/serializer.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
 from kanban_app.models import Task
 
-class TaskSerializer(serializers.Serializer):  
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    category = serializers.CharField(max_length=255)
-    dueDate = serializers.CharField(max_length=255)
-    priority = serializers.CharField(max_length=255)
-    assignedTo = serializers.CharField(max_length=255)
-    subTasks = serializers.CharField(max_length=255)
-    subTasksCompleted = serializers.JSONField()
-    taskStatus = serializers.CharField(max_length=255)
-    taskColumnOrder = serializers.IntegerField()
-    firebaseId = serializers.CharField(max_length=255)
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+# class TaskSerializer(serializers.Serializer):  
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=255)
+#     description = serializers.CharField()
+#     category = serializers.CharField(max_length=255)
+#     dueDate = serializers.CharField(max_length=255)
+#     priority = serializers.CharField(max_length=255)
+#     assignedTo = serializers.CharField(max_length=255)
+#     subTasks = serializers.CharField(max_length=255)
+#     subTasksCompleted = serializers.JSONField()
+#     taskStatus = serializers.CharField(max_length=255)
+#     taskColumnOrder = serializers.IntegerField()
+#     firebaseId = serializers.CharField(max_length=255)
     
-    def create(self, validated_data):
-        return Task.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Task.objects.create(**validated_data)
