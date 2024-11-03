9d9a246a64d8c618db08f8689360599783e9f5ea
Christoph <c.wagner.germany@gmail.com>
2024-11-03 16:48:27 +0100

    new model

--- kanban_app/api/serializer.py
+++ kanban_app/api/serializer.py
@@ -1,24 +1,41 @@
 from rest_framework import serializers
-from kanban_app.models import Task
+from kanban_app.models import User, Task, Subtask
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'user_color', 'type']
+
+class SubtaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subtask
+        fields = ['id', 'title', 'is_completed', 'task']
 
 class TaskSerializer(serializers.ModelSerializer):
+    users = UserSerializer(many=True)  # Nested representation of users
+    subtasks = SubtaskSerializer(many=True, read_only=True)  # Nested subtasks
+    
     class Meta:
         model = Task
-        fields = '__all__'
-
-# class TaskSerializer(serializers.Serializer):  
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(max_length=255)
-#     description = serializers.CharField()
-#     category = serializers.CharField(max_length=255)
-#     dueDate = serializers.CharField(max_length=255)
-#     priority = serializers.CharField(max_length=255)
-#     assignedTo = serializers.CharField(max_length=255)
-#     subTasks = serializers.CharField(max_length=255)
-#     subTasksCompleted = serializers.JSONField()
-#     taskStatus = serializers.CharField(max_length=255)
-#     taskColumnOrder = serializers.IntegerField()
-#     firebaseId = serializers.CharField(max_length=255)
+        fields = [
+            'id', 'title', 'description', 'category', 'due_date', 'priority', 
+            'users', 'status', 'position', 'subtasks'
+        ]
     
-#     def create(self, validated_data):
-#         return Task.objects.create(**validated_data)
+    def create(self, validated_data):
+        users_data = validated_data.pop('users')
+        task = Task.objects.create(**validated_data)
+        for user_data in users_data:
+            user, created = User.objects.get_or_create(**user_data)
+            task.users.add(user)
+        return task
+
+    def update(self, instance, validated_data):
+        users_data = validated_data.pop('users', None)
+        if users_data:
+            instance.users.clear()
+            for user_data in users_data:
+                user, created = User.objects.get_or_create(**user_data)
+                instance.users.add(user)
+        
+        return super().update(instance, validated_data)
--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path, include
-from .views import TaskViewSet
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from . import views
 
-router = routers.SimpleRouter()
-router.register(r'tasks', TaskViewSet)
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'tasks', views.TaskViewSet)
+router.register(r'subtasks', views.SubtaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+# from django.urls import path, include
+# from .views import TaskViewSet
+# from rest_framework import routers
+
+# router = routers.SimpleRouter()
+# router.register(r'tasks', TaskViewSet)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
