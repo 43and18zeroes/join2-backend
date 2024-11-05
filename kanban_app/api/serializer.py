f1ab5c678faef3956f8d57e13af551aaee53d189
Christoph <c.wagner.germany@gmail.com>
2024-11-05 15:57:18 +0100

    user

--- kanban_app/api/serializer.py
+++ kanban_app/api/serializer.py
@@ -14,19 +14,23 @@ class SubtaskSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
     users = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())  # Use IDs for users
     subtasks = SubtaskSerializer(many=True, read_only=True)  # Nested subtasks
+    category_choices = serializers.SerializerMethodField()
     
     class Meta:
         model = Task
         fields = [
             'id', 'title', 'description', 'category', 'due_date', 'priority', 
-            'users', 'status', 'position', 'subtasks'
+            'users', 'status', 'position', 'subtasks', 'category_choices'
         ]
+    
+    def get_category_choices(self, obj):
+        return Task.CATEGORY_CHOICES
     
     def create(self, validated_data):
         users_data = validated_data.pop('users')
         task = Task.objects.create(**validated_data)
         for user_data in users_data:
-            user, created = User.objects.get_or_create(**user_data)
+            user, created = User.objects.get_or_create(id=user_data.id)  # Use user ID to get or create
             task.users.add(user)
         return task
 
@@ -35,7 +39,7 @@ class TaskSerializer(serializers.ModelSerializer):
         if users_data:
             instance.users.clear()
             for user_data in users_data:
-                user, created = User.objects.get_or_create(**user_data)
+                user, created = User.objects.get_or_create(id=user_data.id)  # Use user ID to get or create
                 instance.users.add(user)
         
         return super().update(instance, validated_data)
