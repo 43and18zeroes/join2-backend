aecfbfce0308c8c8aabb83ec762a12c4a8179ee8
Christoph <c.wagner.germany@gmail.com>
2024-12-13 17:24:28 +0100

    cloned endpoint2

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import UserProfileList, UserProfileDetail
+# from .views import UserProfileList, UserProfileDetail
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'registration', views.UserViewSet)
 
 urlpatterns = [
-    path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
-    path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-list'),
+    path('', include(router.urls)),
+    # path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
+    # path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-list'),
 ]
