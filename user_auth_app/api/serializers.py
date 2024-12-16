7c2e3f5fcf46d7d429f620678f4c7318a9db50ae
Christoph <c.wagner.germany@gmail.com>
2024-12-16 16:15:52 +0100

    User created successfully:

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -12,31 +12,35 @@ class UserProfileSerializer(serializers.ModelSerializer):
         }
         
 class RegistrationSerializer(serializers.ModelSerializer):
-    
+    phone_number = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    user_color = serializers.CharField(write_only=True, required=False)
+    type = serializers.ChoiceField(choices=UserProfile.TYPE_CHOICES, write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'user_color', 'type', 'initials', 'password']
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'phone_number', 'user_color', 'type']
         extra_kwargs = {
-            'password': {
-                'write_only': True
-            }
+            'password': {'write_only': True}
         }
-        
+
     def save(self):
-        pw = self.validated_data['password']
-        
-        account = User(
+        user = User(
             first_name=self.validated_data['first_name'],
             last_name=self.validated_data['last_name'],
             email=self.validated_data['email'],
-            phone_number=self.validated_data['phone_number'],
-            user_color=self.validated_data['user_color'],
-            type=self.validated_data['type'],
-            initials=self.validated_data['initials']
         )
-        account.set_password(pw)
-        account.save()
-        return account
+        user.set_password(self.validated_data['password'])
+        user.save()
+
+        # Create a UserProfile for the user
+        UserProfile.objects.create(
+            user=user,
+            phone_number=self.validated_data.get('phone_number', ''),
+            user_color=self.validated_data.get('user_color', ''),
+            type=self.validated_data.get('type', 'user_from_signup')
+        )
+
+        return user
 
 # class UserProfileSerializer(serializers.ModelSerializer):
 #     class Meta:
